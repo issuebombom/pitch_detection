d635d9bddcf9c381eb2529c3a1e65c7966102406
issuebombom <issuebombom@gmail.com>
2022-12-13 14:24:54 +0900

    Feat: 피아노 pitch를 detection하는 프로그램 개발

---
+++ models.py
@@ -0,0 +1,63 @@
+import librosa
+import numpy as np
+import pickle
+import statsmodels.api as sm
+from scipy.signal import find_peaks
+from utils import *
+
+
+def run_model(audio_file):
+
+    hertz2keys = load_pickle("hertz2keys.pkl")
+
+    # 음원 길이 설정(초)
+    duration = 20
+    data, sample_rate = librosa.load(audio_file, sr=44100, mono=True, duration=duration)
+
+
+    # 0.01초 단위로 데이터 슬라이싱
+    sec = 0.01
+    trim_sec = int(1 / sec)
+    n_rows = data.shape[0] // sample_rate * trim_sec # 지정한 주기로 슬라이싱
+    dataset = data.reshape(n_rows, -1)
+
+    detected_hertz = []
+    for sample in dataset:
+        if sample.mean() == 0: # 구간 내 소리가 없는 경우 0 입력
+            detected_hertz.append(0) # No Signal
+            continue
+        
+        autocorrelation = sm.tsa.acf(sample, nlags=200)
+        peaks = find_peaks(autocorrelation)[0] # Find peaks of the autocorrelation (threshold=0.001)
+        
+        if peaks.shape[0] == 0: # peak가 없는 경우 0 입력
+            detected_hertz.append(0) # No Peak
+            continue
+        
+        lag = peaks[0] # Choose the first peak as our pitch component lag
+        pitch = int(sample_rate / lag) # Transform lag into frequency
+        
+        detected_hertz.append(find_closed_key(pitch)) # change hertz to closed key hertz
+
+        
+    # task 1
+    # 0.1초 단위로 가장 빈도수가 높은 키 하나만 남기기
+    sec = 0.1
+    trim_sec = int(1 / sec)
+    n_rows = duration * trim_sec
+    result = np.array(detected_hertz).reshape(n_rows, -1) # (220, 10)
+
+    freq_hertz_result = np.array([freq_check(array) for array in result])
+
+
+    # task 2
+    # n개 단위로 그룹화한 뒤 빈도수 낮은 key값을 제외
+    group_size = 10
+    drop_rate = 0.1
+    reshaped = freq_hertz_result.reshape(-1, group_size) # (22, 10)
+    final_result = freq_check2(reshaped, drop_rate) # length의 10% 이하 빈도수 key값은 제외
+
+    final_result = [hertz2keys[hertz] for hertz in final_result]
+    final_result = delete_continuous_value(final_result)
+
+    return final_result
---
+++ utils.py
@@ -0,0 +1,89 @@
+import pickle
+import numpy as np
+from collections import Counter
+
+def load_pickle(path):
+    """Load json file
+
+    Args:
+        path (str): input file directory about json file
+
+    Returns:
+        dict: json file to dictionary
+    """
+    with open(path, 'rb') as fr:
+        pickle_data = pickle.load(fr)
+    
+    return pickle_data
+
+def find_closed_key(hertz):
+    """음원이 계이름과 정확하게 일치하는 hertz를 출력하지 않으므로 근사하는 계이름을 출력하는 도구
+
+    Args:
+        hertz (float): input hertz for change neighborhood key
+
+    Returns:
+        float: output neighborhood key close with hertz you input
+    """
+    hertz2keys = load_pickle("hertz2keys.pkl")
+    if hertz == 0:
+        return 0.0
+    
+    herts_array = np.array(list(hertz2keys.keys())) # 딕셔너리 key값을 리스트로 변경
+    closed_index = np.argmin(abs(herts_array - hertz)) # 확인 대상과 가장 가까운 계이름 hertz 찾기
+    
+    return herts_array[closed_index] # 출력
+
+
+
+def freq_check(array):
+    """np.array에서 빈도수가 가장 높은 원소를 출력
+
+    Args:
+        array (np.array): 최고 빈도수 원소 추출을 위한 array 입력
+
+    Returns:
+        int: 빈도수가 가장 높은 값을 출력
+    """
+    freq_result = np.unique(array, return_counts=True) # (array([ 980, 1002, 1260]), array([1, 8, 1])) 튜플 반환
+    result = freq_result[0][np.argmax(freq_result[1])] # 최대 빈도수 인덱싱
+    
+    return result # int
+
+
+def freq_check2(array, threshold:float):
+    """범위 내 요소별 빈도수 체크 및 일정 빈도수 이하 요소 제외
+
+    Args:
+        array (np.array): 빈도수 체크를 위한 array 입력
+        threshold (float): array 길이의 n 비율에 해당하는 값 이하의 빈도수를 가진 key 제외
+
+    Returns:
+        list: 낮은 빈도수를 갖는 원소가 제외된 리스트를 출력합니다.
+    """
+    result = []
+    for hertz_list in array:
+        counter = Counter(hertz_list)
+        temp = [hertz for hertz in hertz_list if counter[hertz] > hertz_list.shape[0] * threshold]
+        result = result + temp
+    return result # list
+
+
+def delete_continuous_value(array:list):
+    """리스트 내 연속된 중복값을 제거합니다.
+
+    Args:
+        array (list): 중복값 제거 대상 리스트 입력
+
+    Returns:
+        list: 연속 중복값을 제외한 리스트 출력
+    """
+    curr_key = ""
+    new_list = []
+    for key in array:
+        if curr_key == key:
+            continue
+        new_list.append(key)
+        curr_key = key
+        
+    return new_list
