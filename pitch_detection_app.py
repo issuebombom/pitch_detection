57ba10284e142946b81422c4fa60f8742b8c5f2f
issuebombom <issuebombom@gmail.com>
2022-12-14 17:06:08 +0900

    Chore: minor

--- pitch_detection_app.py
+++ pitch_detection_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from models import run_model
 
 st.title('Pitch Detection')
-uploaded_file = st.file_uploader("Import your mp3 file", type=['wav'])
+uploaded_file = st.file_uploader("Import your audio file", type=['wav', 'mp3'])
 
 if uploaded_file is not None:
-    st.audio(uploaded_file, format='audio/wav') # mp3 적용 시 wav로 변환해주는 작업 추가 필요
+    st.audio(uploaded_file, format='audio/wav')
     run_model = run_model(uploaded_file)
 
 # 버튼을 클릭하면 탐지한 계이름을 시퀀셜하게 출력해 줍니다.
