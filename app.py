dbd441d952d102ad7f34ab24f3af1d281c3fc313
issuebombom <issuebombom@gmail.com>
2022-12-14 13:39:40 +0900

    Chore: add.py파일 이름을 pitch_detection_app으로 변경

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from models import run_model
-
-st.title('Pitch Detection')
-audio_file = st.file_uploader("Import your mp3 file")
-run_model = run_model(audio_file)
-
-st.button('push button to run', on_click=run_model)
